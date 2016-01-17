a782985f9884cfd39b562debe53036e013dc64cb
Per Sundberg <per.sundberg@polystar.com>
2016-01-17 02:52:38 +0100

    update of game-server index.php file

---
+++ game-server/gameheader.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang='en' class='no-js'>
+<head>
+<meta charset='utf-8' />
+<title><?=isset($title) ? $title : 'Jägarna och Vargen'?></title>
+<link rel="stylesheet/less" type="text/css" href="style.less">
+<script src="js/less.min.js"></script>
+<script src="js/modernizr.js"></script>
+</head>
+<body>
